9dbf47d04c50ca96f7ad8a816d568837880f3bfe
wrimoldi-byte <wrimoldi@gmail.com>
2026-08-06 02:47:38 -0300

    Usar interfaz principal con burbuja flotante

--- AEPControl/Program.cs
+++ AEPControl/Program.cs
@@ -6,6 +6,6 @@ internal static class Program
     private static void Main()
     {
         ApplicationConfiguration.Initialize();
-        Application.Run(new MainForm());
+        Application.Run(new BubbleMainForm());
     }
 }
