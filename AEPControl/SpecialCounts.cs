72b6071f9b56a5cc3d722a8c9b689c64d460d4ac
wrimoldi-byte <wrimoldi@gmail.com>
2026-08-18 06:09:02 -0300

    Count extended special service codes

--- AEPControl/SpecialCounts.cs
+++ AEPControl/SpecialCounts.cs
@@ -7,5 +7,21 @@ public sealed class SpecialCounts
     public int WCHC { get; set; }
     public int AVIH { get; set; }
     public int INF { get; set; }
-    public int Total => WCHR + WCHS + WCHC + AVIH + INF;
+
+    public int UMNR { get; set; }
+    public int PETC { get; set; }
+    public int DEAF { get; set; }
+    public int BLND { get; set; }
+    public int MAAS { get; set; }
+    public int STCR { get; set; }
+    public int MEDA { get; set; }
+    public int WCLBD { get; set; }
+    public int WCMP { get; set; }
+    public int SVAN { get; set; }
+    public int ESAN { get; set; }
+    public int INAD { get; set; }
+    public int DEPA { get; set; }
+    public int DEPU { get; set; }
+
+    public int Total => WCHR + WCHS + WCHC + AVIH + INF + UMNR + PETC + DEAF + BLND + MAAS + STCR + MEDA + WCLBD + WCMP + SVAN + ESAN + INAD + DEPA + DEPU;
 }
